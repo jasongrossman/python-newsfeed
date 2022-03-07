860ade9dbbefe94126f02949e992f5e71d66ed6b
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2022-03-07 15:24:21 -0500

    added signup route logic

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,12 +1,35 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 @bp.route('/users', methods=['POST'])
 def signup():
     data = request.get_json()
-    print(data)
+    db = get_db()
+    
+    try:
+        # create a new user
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
 
-    return ''
+        # save in database
+        db.add(newUser)
+        db.commit()
+    except:
+        # insert failed, so send error to front end
+        print(sys.exc_info()[0])
+
+        # insert failed, so rollback and send error to front end
+        db.rollback()
+        return jsonify(message = 'Signup failed'), 500
+
+    session.clear()
+    session['user_id'] = newUser.id
+    session['loggedIn'] = True
+    return jsonify(id = newUser.id)
